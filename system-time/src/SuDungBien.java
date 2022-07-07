6c0917301874511e63a6d3a82716273dd6ee3b87
nguyenhuytruc <huytruc1201@gmail.com>
2022-07-07 14:13:43 +0700

    khai bao va su dung bien

---
+++ system-time/src/SuDungBien.java
@@ -0,0 +1,17 @@
+public class SuDungBien{
+    public static void main(String[] args) {
+        String name = "Nguyễn Huy Trúc";
+        double weight = 63.5;
+        float height = 1.75F;
+        int age = 22;
+        boolean single = true;
+        char address = 'h';
+        System.out.println("name: " + name);
+        System.out.println("weight: " + weight);
+        System.out.println("height: " + height);
+        System.out.println("age: " + age);
+        System.out.println("single: " + single);
+        System.out.println("address: " + address);
+    }
+}
+
