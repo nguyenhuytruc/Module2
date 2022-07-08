d72fe7e53b3c6288615eb8e40e2b41ec25f7224f
nguyenhuytruc <huytruc1201@gmail.com>
2022-07-08 14:50:57 +0700

    tim USCLN

---
+++ loop/src/FindGCD.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FindGCD {
+    public static void main(String[] args) {
+        int num1, num2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số thứ nhất:");
+        num1 = scanner.nextInt();
+        System.out.print("Nhập số thứ hai:");
+        num2 = scanner.nextInt();
+        while (num1 != num2) {
+            if (num1 > num2)
+                num1 = num1 - num2;
+            else
+                num2 = num2 - num1;
+        }
+        System.out.printf("Ước chung lớn nhất là: %d", num2);
+    }
+}
+
