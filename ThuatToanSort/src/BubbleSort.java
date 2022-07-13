b5c2f4282a5465f6083ae32b96a78a4309772b50
nguyenhuytruc <huytruc1201@gmail.com>
2022-07-13 15:49:26 +0700

    thuat toan sap xep noi bot

---
+++ ThuatToanSort/src/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    static int[] list = {34, 54, 43, 35, 65, -3, 2, 5, 67, 23};
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
